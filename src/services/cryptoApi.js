65a43dcaab63e5be8781c1f759e87d900c14853a
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-19 21:46:10 +0600

    redux toolkit & API dev

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '897b864698mshd9566898364e02dp11e5e2jsn05a7bf77f30a'
+
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+export const cryptoApi = createApi({
+
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest ('/exchanges')
+        })
+    })
+});
