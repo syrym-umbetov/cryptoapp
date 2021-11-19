542bf928601277ef5e798466722651e6b10676d9
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-19 22:36:43 +0600

    globalStats

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,14 +3,18 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 
+import { useGetCryptosQuery } from '../services/cryptoApi';
 const { Title } = Typography;
 
 const Homepage = () => {
+    const { data, isFetching } = useGetCryptosQuery(10);
+    const globalStats = data?.data?.stats;
+    if(isFetching) return 'Loading ... ';
     return (
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
-                <Col span={12}><Statistic title="Total CryptoCurrencies" value="5"/></Col>
+                <Col span={12}><Statistic title="Total CryptoCurrencies" value={globalStats.total}/></Col>
                 <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
                 <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
                 <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
@@ -16,7 +16,11 @@ export const cryptoApi = createApi({
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
         getCryptos: builder.query({
-            query: () => createRequest ('/exchanges')
+            query: () => createRequest ('/coins')
         })
     })
 });
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
