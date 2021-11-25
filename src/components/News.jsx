f75272d1dd6f1d3cd3f132c2114bce0fce7f8388
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-25 20:34:39 +0600

    news avatar text images

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,6 +8,8 @@ const { Text, Title } = Typography;
 
 const { Option } = Select;
 
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 const News = ({ simplified }) => {
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'CryptoCurrency', count: simplified ? 6 : 12 })
     
@@ -21,6 +23,19 @@ const News = ({ simplified }) => {
                         <a href={news.url} target="_blank" rel="noreferrer">
                             <div className="news-image-container">
                                 <Title className="news-title" level={4}>{news.name}</Title>
+                                <img style={{ maxWidth: '200px', maxHeight: '100px'}}src={news?.image?.thumbnail?.contentUrl || demoImage } alt="news" />
+                            </div>
+                            <p>
+                                {news.description > 100 ? `${news.description.substring(0,100)}...`
+                                : news.description
+                                }
+                            </p>
+                            <div className="provider-container">
+                                <div>
+                                    <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                                    <Text className="provider-name">{news.provider[0]?.name}</Text>
+                                </div>
+                                <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                             </div>
                         </a>
                     </Card>
