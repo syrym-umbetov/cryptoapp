b443b48c3d429589d54ecf76bd7ce4669cad487a
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-25 20:21:32 +0600

    latest crypto news

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -15,16 +15,15 @@ const Cryptocurrencies = ({simplified}) => {
         const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
         setCryptos(filteredData);
     }, [cryptosList, searchTerm]);
-
-        
-
+    
     if(isFetching) return 'Loading...';
 
     return (
         <>
+            {!simplified &&(
             <div className="search-crypto">
                 <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
-            </div>
+            </div>)}
             <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoNewsHeaders = {
+        'x-bingapis-sdk': 'true',
+        'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+        'x-rapidapi-key': '897b864698mshd9566898364e02dp11e5e2jsn05a7bf77f30a'
+      }
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders})
+
+export const cryptoNewsApi = createApi({
+
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => createRequest (`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
