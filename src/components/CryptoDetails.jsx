2ced9daddb9dd24573fe0832c5ffb3447ae7b393
syrym-umbetov <umbetov.syrym@gmail.com>
2021-11-29 15:43:16 +0600

    Links, description

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -6,29 +6,30 @@ import { Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
 import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/cryptoApi';
-
+import Loader from './Loader';
 const { Title, Text } = Typography;
 const { Option } = Select;
 
 
 const CryptoDetails = () => {
     const { coinId } = useParams();
-    console.log("coinId", coinId)
-    const [ timePeriod, setTimePeriod] = useState('7d')
+    
+    const [ timeperiod, setTimePeriod] = useState('7d')
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
-    console.log("data", data)
+    const { data: coinHistory } = useGetCryptoHistoryQuery({ coinId, timeperiod });
     const cryptoDetails = data?.data?.coin;
     
-    console.log("cryptoDetails", cryptoDetails);
+    if (isFetching) return <Loader />;
+
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     let stats;
     if (cryptoDetails) {
     stats = [
-            {title: 'Price to USD', value: `$sdfsd ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+            { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
             { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-            { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
-            { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+            { title: '24h Volume', value: `$ ${cryptoDetails.price && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+            { title: 'Market Cap', value: `$ ${cryptoDetails.price && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
             { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
         ]
     }
@@ -48,16 +49,20 @@ const CryptoDetails = () => {
 
     return (
         <Col className="coin-detail-container">
-            <Col className="coin-heading-container">
-                <Title level={2} className="coin-name">
-                    {cryptoDetails && cryptoDetails.name} ({cryptoDetails && cryptoDetails.slug}) Price
-                </Title>
-            
-            <p>
-                {cryptoDetails && cryptoDetails.name} live price in US dollars.
-                View value statistics, market cap and supply.
-            </p>
-            </Col>
+            {cryptoDetails && (
+                <>
+                    <Col className="coin-heading-container">
+                        <Title level={2} className="coin-name">
+                            {cryptoDetails.name} ({cryptoDetails.slug}) Price
+                        </Title>
+                
+                        <p>
+                            {cryptoDetails.name} live price in US dollars.
+                            View value statistics, market cap and supply.
+                        </p>
+                    </Col>
+                </>
+            )}
            <Select
                 defaultValue="7d" 
                 className="select-timerperiod" 
@@ -68,22 +73,71 @@ const CryptoDetails = () => {
             </Select>
             <Col className="stats-container">
                 <Col className="coin-value-statistics">
-                    <Col className="coin-value-statistics-heading">
+                    {cryptoDetails && (
+                        <Col className="coin-value-statistics-heading">
                         <Title level={3} className="coin-details-heading">
-                            {cryptoDetails && cryptoDetails.name} Value Statistics
+                            {cryptoDetails.name} Value Statistics
                         </Title>
                         <p>
                             An overview showing the stats of {cryptoDetails && cryptoDetails.name}
                         </p>
                     </Col>
+                    )}
+                    
                     {cryptoDetails && cryptoDetails.stats && cryptoDetails.stats.map(({ icon, title, value}) => (
                         <Col className="coin-stats">
                             <Col className="coin-stats-name">
                                 <Text>{icon}</Text>
                                 <Text>{title}</Text>
-                            </Col>                                   
+                            </Col>
+                            <Text className="stats">{value}</Text>                                   
                         </Col>
                     ))}
+                </Col>
+                <Col className="other-stats-info">
+                    {cryptoDetails && (
+                        <Col className="coin-value-statistics-heading">
+                        <Title level={3} className="coin-details-heading">
+                            {cryptoDetails.name} Other Statistics
+                        </Title>
+                        <p>
+                            An overview showing the stats of all cryptocurrencies
+                        </p>
+                    </Col>
+                    )}
+                    
+                    {cryptoDetails && cryptoDetails.stats && cryptoDetails.genericStats.map(({ icon, title, value}) => (
+                        <Col className="coin-stats">
+                            <Col className="coin-stats-name">
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                            </Col>
+                            <Text className="stats">{value}</Text>                                   
+                        </Col>
+                    ))}
+                </Col>
+            </Col>
+            <Col className="coin-desc-link">
+                <Row className="coin-desc">
+                        <Title level={3} className="coin-details-heading">
+                            What is {cryptoDetails.name}
+                            {HTMLReactParser(cryptoDetails.description)}
+                        </Title>
+                </Row>
+                <Col className="coin-links">
+                    <Title level={3} className="coin-details-heading">
+                        {cryptoDetails.name} Links            
+                    </Title>
+                    {cryptoDetails.links.map((link) =>
+                        <Row className="coin-link" key={link.name}>
+                            <Title level={5} className="link-name">
+                                {link.type}
+                            </Title>
+                            <a href={link.url} target="_blank" rel="noreferrer">
+                                {link.name}
+                            </a>
+                        </Row> 
+                    )}
                 </Col>
             </Col>
         </Col>
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -8,7 +8,6 @@ const cryptoApiHeaders = {
 
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 
-
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 export const cryptoApi = createApi({
 
@@ -18,12 +17,18 @@ export const cryptoApi = createApi({
         getCryptos: builder.query({
             query: (count) => createRequest (`/coins?limit=${count}`),
         }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+          }),
         getCryptoDetails: builder.query({
             query: (coinId) => createRequest (`/coin/${coinId}`),
-        })
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+        }),
     })
 });
 
 export const {
-    useGetCryptosQuery, useGetCryptoDetailsQuery
+    useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery
 } = cryptoApi;
